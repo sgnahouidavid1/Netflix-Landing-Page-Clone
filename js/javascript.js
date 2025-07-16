c5c2e656a61252cc3e22bbda2f7d28c5cf55d115
sgnahouidavid1 <81520639+sgnahouidavid1@users.noreply.github.com>
2025-07-16 00:26:31 -0400

    Finish making the landing page
    Finish creating the landing page of a Netflix website clone.

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,23 @@ selectorButton.addEventListener('click', () => {
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 });
 
-// Add logic to handle language selection from the dropdown options
+// Add logic to handle language selection from the dropdown options here.
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqGroupContainer = document.querySelector('.faq-content')
+    faqGroupContainer.addEventListener('click', (e) => {
+        const faqGroup = e.target.closest('.faq-group')
+        if (!faqGroup) return;
+        const icon = faqGroup.querySelector('.faq-icon-btn')
+        const faqgroupBody = faqGroup.querySelector('.faq-group-body')
+        
+        
+        icon.classList.toggle('active');
+        faqgroupBody.classList.toggle('open');
+
+    });
+});
